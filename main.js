3f5f3218d7ac1858ac5809a7b30d8398e561c533
JeffSchultz99 <jeffschultz99@gmail.com>
2022-01-11 14:27:28 -0800

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./halo.png");
 ASSET_MANAGER.queueDownload("./skeleton.png");
+ASSET_MANAGER.queueDownload("./dragon.png");
+ASSET_MANAGER.queueDownload("./bird.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
