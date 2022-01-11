62cc628059c962e70da23adec1deefe270c6abce
JeffSchultz99 <jeffschultz99@gmail.com>
2022-01-10 18:21:21 -0800

    fixed image

--- skeleton.js
+++ skeleton.js
@@ -1,7 +1,7 @@
 class skeleton {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./skeleton.png"), 0, 0, 60.3, 52, 10, 0.5);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./skeleton.png"), 0, 0, 60.3, 52, 9, 0.2);
     }
 
     update() {
@@ -9,6 +9,6 @@ class skeleton {
     }
 
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick, ctx, 100, 100);
+        this.animator.drawFrame(this.game.clockTick, ctx, 250, 250);
     }
 }
